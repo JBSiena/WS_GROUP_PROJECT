4ac7a4df71a15758c5f756c10539ea7679377afd
jbsiena <jbsiena05@gmail.com>
2024-11-16 04:15:31 +0800

    Check niyo

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'static'),
-            os.path.join(BASE_DIR, 'media')
+            os.path.join(BASE_DIR, 'media'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
